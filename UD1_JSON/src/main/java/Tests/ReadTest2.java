21c622b0073a20a3df693d36565537c7c9981557
maavcode <maavcode@hotmail.com>
2025-10-06 14:12:13 +0200

    correcion

--- UD1_JSON/src/main/java/Tests/ReadTest2.java
+++ UD1_JSON/src/main/java/Tests/ReadTest2.java
@@ -14,16 +14,15 @@ public class ReadTest2 {
 
 	public static void main(String[] args) {
 		JSONParser parser = new JSONParser();
-		JSONArray jArray = null;
 		try {
 			// Guardamos fichero en un Object ya que empieza por {
-			Object obj = parser.parse(new FileReader("ficheros\\test2.json"));
-			JSONObject jObj = (JSONObject)obj; // Convertimos el Object en JSONObject
+			Object objTest2 = parser.parse(new FileReader("ficheros\\test2.json"));
+			JSONObject jObjTest2 = (JSONObject)objTest2; // Convertimos el Object en JSONObject
 			
 			// Ejercicios
-			ejercicio01(jObj, jArray);
-			ejercicio02(jObj, jArray);
-			ejercicio03(jObj, jArray);
+			ejercicio01(jObjTest2);
+			ejercicio02(jObjTest2);
+			ejercicio03(jObjTest2);
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -37,40 +36,44 @@ public class ReadTest2 {
 		}
 	}
 	
-	public static void ejercicio01(JSONObject jObj, JSONArray jArray) {
+	public static void ejercicio01(JSONObject jObjTest2) {
 		System.out.println("--- Mostrar URLs ---");
-		jArray = (JSONArray) jObj.get("pages"); // Lo que queremos recoger es un array de campos
-		for (Object object : jArray) {
-			jObj = (JSONObject)object;
-			System.out.println(jObj.get("pageUrl"));
+		JSONArray jArrayPages = (JSONArray) jObjTest2.get("pages"); // Lo que queremos recoger es un array de campos
+		for (Object object : jArrayPages) {
+			JSONObject jObjPage = (JSONObject)object;
+			System.out.println(jObjPage.get("pageUrl"));
 		}
 	}
 	
-	public static void ejercicio02(JSONObject jObj, JSONArray jArray) {
+	public static void ejercicio02(JSONObject jObjTest2) {
 		System.out.println("--- Mostrar results ---");
-		jArray = (JSONArray) jObj.get("pages");
+		JSONArray jArrayPages = (JSONArray) jObjTest2.get("pages");
 		// Recorro el array pages
-		for (Object object : jArray) {
-			jObj = (JSONObject) object;
-			System.out.println(jObj.get("pageUrl"));
-			jArray = (JSONArray) jObj.get("results");
-			for (Object object2 : jArray) { // Recorre el array results en cada iteracion de pages
-				jObj = (JSONObject) object2;
-				System.out.println(jObj.get("number"));
+		for (Object object : jArrayPages) {
+			JSONObject jObjPage = (JSONObject) object;
+			System.out.println(jObjPage.get("pageUrl"));
+			JSONArray jArrayResults = (JSONArray) jObjPage.get("results");
+			for (Object object2 : jArrayResults) { // Recorre el array results en cada iteracion de pages
+				JSONObject jObjResult = (JSONObject) object2;
+				System.out.println(jObjResult.get("number"));
 			}
 		}
 	}
 	
-	public static void ejercicio03(JSONObject jObj, JSONArray jArray) {
+	public static void ejercicio03(JSONObject jObjTest2) {
 		System.out.println("--- Mostrar results de example0 ---");
-		jArray = (JSONArray) jObj.get("pages");
-		jObj = (JSONObject) jArray.getFirst();
-		System.out.println(jObj.get("pageUrl"));
-		jArray = (JSONArray) jObj.get("results");
-		for (Object object : jArray) {
-			jObj = (JSONObject) object;
-			System.out.println(jObj.get("number"));
+		JSONArray jArrayPages = (JSONArray) jObjTest2.get("pages");
+		JSONObject jObjFirstPage = (JSONObject) jArrayPages.getFirst();
+		System.out.println(jObjFirstPage.get("pageUrl"));
+		JSONArray jArrayrResults = (JSONArray) jObjFirstPage.get("results");
+		for (Object object : jArrayrResults) {
+			JSONObject jObjResult = (JSONObject) object;
+			System.out.println(jObjResult.get("number"));
 		}
+	}
+	
+	public static void ejercicio04() {
+		
 	}
 
 	
